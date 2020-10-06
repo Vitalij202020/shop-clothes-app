a8aaf44f102e876da4c9d8c43ce7bafe70fa3930
Vitalij202020 <vaskouski.v.v@gmail.com>
2020-10-06 12:52:37 +0300

    added redux and implemented userReduser and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
+import {connect} from 'react-redux'
+import {setCurrentUser} from './redux/user/user.actions'
 
 class App extends React.Component {
   constructor() {
     super()
-
     this.state = {
       currentUser: null,
     }
@@ -22,7 +23,6 @@ class App extends React.Component {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
-
         userRef.onSnapshot((snapShot) => {
           this.setState({
             currentUser: {
@@ -56,4 +56,6 @@ class App extends React.Component {
   }
 }
 
-export default App
+const mapDispatchToProps = (dispatch) => ({setCurrentUser: (user) => dispatch(setCurrentUser(user))})
+
+export default connect(null, mapDispatchToProps)(App)
